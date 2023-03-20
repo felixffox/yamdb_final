8b7c515bf61d2328c1d0879e7c456400349d2713
Danil Vdovin <felixffox@yandex.ru>
2023-03-20 21:01:20 +0400

    First commit fo sprint

--- api_yamdb/users/permissions.py
+++ api_yamdb/users/permissions.py
@@ -7,3 +7,5 @@ class RoleAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.user.is_authenticated and request.user.role == "admin":
             return True
+        else:
+            return None
